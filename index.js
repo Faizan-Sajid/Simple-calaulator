20eacb1ec5b1715bcdccb4528291857378353158
faizan sajid <165155018+Faizan-Sajid@users.noreply.github.com>
2024-05-19 04:48:55 -0700

    My first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "division", "multiply"],
+    },
+]);
+if (answer.operator == "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiply") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumberS);
+}
+else {
+    console.log("please select valid number");
+}
